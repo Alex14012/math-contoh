797c9d66dcccea0a201232e7e331833113b9ddb6
DGL112 <legendgg211@gmail.com>
2023-11-23 12:19:39 +0700

    first commit

---
+++ contoh math/script.js
@@ -0,0 +1,46 @@
+let a = 6.6;
+document.write("variable a = 6.6 <br>");
+let b = 2.4;
+document.write("variable a = 2.4 <br>"); 
+let c = -5.8;
+document.write("varible = -5.8 <br><br>");
+
+let bulatkan = Math.round(a);
+let bulatkan1 = Math.round(b);
+let bulatkan2 = Math.round (c);
+document.write(`Math.round => a dibulatkan = ${bulatkan}, b dibulatkan = ${bulatkan1}, dan c dibulatkan = ${bulatkan2} <br> <br>`);
+
+let x = 5.9; 
+document.write("variable x = 5.9 <br>");
+let y = 5.6; 
+document.write("variable y = 5.6 <br>");
+let z = -5.5;
+document.write("variable z = -5.5 <br><br>")
+
+let floor = Math.floor(x);
+let floor1 = Math.floor(y);
+let floor2 = Math.floor(z);
+document.write(`Math.floor => x dibulatkan ke bawah = ${floor}, y dibulatkan ke bawah = ${floor1}, dan z dibulatkan ke bawah = ${floor2} <br><br>`);
+
+let a1 = 2.5; 
+document.write("variable a1 = 2.5 <br>");
+let a2 = 3.1; 
+document.write("variable a2 = 3.1 <br>");
+let a3 = -2.2;
+document.write("variable a3 = -2.2 <br>");
+
+let ceil = Math.ceil(a1);
+let ceil1 = Math.ceil(a2);
+let ceil2 = Math.ceil(a3);
+document.write(`Math.ceil => a1 dibulatkan ke atas = ${ceil}, a2 dibulatkan ke atas = ${ceil1}, dan a3 dibulatkan ke atas = ${ceil2} <br><br>`);
+
+
+let pangkat = Math.pow(3, 2);
+let pangkat1 = Math.pow(5, 3);
+let pangkat2 = Math.pow(6, 2);
+document.write(`Math.pow => 3 pangkat 2 = ${pangkat},  5 pangkat 3 = ${pangkat1}, dan 6 pangkat 2 = ${pangkat2} <br><br>`);
+
+let akar = Math.sqrt(4);
+let akar1 = Math.sqrt(64);
+let akar2 = Math.sqrt(81);
+document.write(`Math.pow => akar 4 = ${akar}, akar 64 = ${akar1}, dan akar 81 = ${akar2} <br><br>`);
